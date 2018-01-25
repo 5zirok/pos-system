1c4308898f5efac52aadd3d2b5ff7a12a5b93af9
Ryabtsev Andrey <zirok@email.ua>
2018-01-25 23:06:45 +0200

    обновление README.MD

--- server/index.js
+++ server/index.js
@@ -6,6 +6,23 @@ path 		= require('path'),
 bodyParser 	= require('body-parser'),
 publicPath 	= '/../public/',
 liveCart
+var usbScanner =  require ('usbscanner'). usbScanner ;
+var getDevices =  require ('usbscanner'). getDevices ;
+//get array of attached HID devices
+var connectedHidDevices = getDevices()
+
+//print devices
+console.log(connectedHidDevices)
+
+//initialize new usbScanner - takes optional parmeters vendorId and hidMap - check source for details
+var scanner = new usbScanner();
+
+//scanner emits a data event once a barcode has been read and parsed
+scanner.on("data", function(code){
+	console.log("recieved code : " + code);
+});
+
+
 global.group_id=3;
 global.usr="od_1";
 
